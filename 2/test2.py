861631fbddeb9da10fbbbbfc6b378d0626e0729d
Luciagase <113980773+Luciagase@users.noreply.github.com>
2023-06-21 09:30:41 +0200

    test2

---
+++ 2/test2.py
@@ -0,0 +1,17 @@
+import pytest
+from ej2 import Armadura
+
+def test_calificacion_armaduras():
+    # Crear una lista con objetos Armadura
+    armaduras = [
+        Armadura("Mark-42", "3-7"),
+        Armadura("Mark-50", "5-2"),
+        Armadura("Mark-7", "1-9"),
+    ]
+
+    # Recorrer los elementos de la lista y ejecutar el método calificacion en cada objeto
+    for armadura in armaduras:
+        armadura.calificacion()
+
+# Ejecutar el test
+pytest.main()
