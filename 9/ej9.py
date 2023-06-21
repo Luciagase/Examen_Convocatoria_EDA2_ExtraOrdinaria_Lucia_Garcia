7254a690aa3caf99b99ca2af64a3172b2f806bb6
Luciagase <113980773+Luciagase@users.noreply.github.com>
2023-06-21 08:59:00 +0200

    grafo

--- 9/ej9.py
+++ 9/ej9.py
@@ -5,3 +5,60 @@ class Planet:
         self.distance = {}
         self.min_distance = float("inf")
         self.previous = None
+
+    def add_neighbor(self, planet, distance):
+        self.neighbors.append(planet)
+        self.distance[planet] = distance
+
+def prim(graph, start_planet):
+    included_planets = set()
+    tree_edges = []
+    included_planets.add(start_planet)
+    while len(included_planets) < len(graph):
+        min_edge_weight = float("inf")
+        min_edge = None
+        for planet in included_planets:
+            for neighbor in planet.neighbors:
+                if neighbor not in included_planets:
+                    if planet.distance[neighbor] < min_edge_weight:
+                        min_edge_weight = planet.distance[neighbor]
+                        min_edge = (planet, neighbor)
+        included_planets.add(min_edge[1])
+        tree_edges.append(min_edge)
+    return tree_edges
+
+def dijkstra(graph, start_planet, end_planet):
+    for planet in graph:
+        planet.min_distance = float("inf")
+    start_planet.min_distance = 0
+    unvisited_planets = list(graph)
+    while unvisited_planets:
+        current_planet = min(unvisited_planets, key=lambda x: x.min_distance)
+        if current_planet == end_planet:
+            break
+        unvisited_planets.remove(current_planet)
+        for neighbor in current_planet.neighbors:
+            if neighbor in unvisited_planets:
+                distance = current_planet.distance[neighbor]
+                if current_planet.min_distance + distance < neighbor.min_distance:
+                    neighbor.min_distance = current_planet.min_distance + distance
+                    neighbor.previous = current_planet
+    path = []
+    planet = end_planet
+    while planet:
+        path.append(planet)
+        planet = planet.previous
+    path.reverse()
+    return path
+
+def bfs(graph, start_planet):
+    visited_planets = set()
+    queue = [start_planet]
+    visited_planets.add(start_planet)
+    while queue:
+        current_planet = queue.pop(0)
+        for neighbor in current_planet.neighbors:
+            if neighbor not in visited_planets:
+                visited_planets.add(neighbor)
+                queue.append(neighbor)
+    return visited_planets
