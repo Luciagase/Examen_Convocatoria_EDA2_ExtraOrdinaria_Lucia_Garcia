52c0640d06fed84275dc4f288a01dfc54127b434
Luciagase <113980773+Luciagase@users.noreply.github.com>
2023-06-21 09:27:27 +0200

    calificación

--- 2/ej2.py
+++ 2/ej2.py
@@ -3,3 +3,17 @@ class Armadura:
         self.nombre = nombre
         self.rango = rango
         print("Armadura creada con éxito")
+
+    def calificacion(self):
+        codigo_legion = self.nombre[:3]
+        identificacion_coherente = len(self.nombre) % 2 == 0
+        identificacion_siglo = self.nombre[0].isdigit()
+        numero_armadura = int(self.rango.split('-')[1])
+        numero_escuadra = int(self.rango.split('-')[0])
+
+        print("Clasificación de la armadura:")
+        print("Código de legión:", codigo_legion)
+        print("Identificación coherente:", identificacion_coherente)
+        print("Identificación de siglo:", identificacion_siglo)
+        print("Número de armadura:", numero_armadura)
+        print("Número de escuadra:", numero_escuadra)
